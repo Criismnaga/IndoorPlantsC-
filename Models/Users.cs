2ba47ffea96d03b045a2214c39fc6af179dd1343
Cristina Matsunaga <criismnaga@gmail.com>
2025-05-23 19:18:46 -0300

    Modelos de Dominio (classes) de Plants.cs e Users.cs criadas

---
+++ Models/Users.cs
@@ -0,0 +1,11 @@
+namespace IndoorPlantsC_.Models;
+
+public class Users
+{
+    string Name { get; set; }
+    string LastName { get; set; }
+    DateTime BirthdayDate { get; set; }
+    string ResidenceType { get; set; }
+    double AvailableTime { get; set; }
+    int ExperiencELevel { get; set; }
+}
