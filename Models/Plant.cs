073261cc95a36be364c2a7f9ec431e9e7ebe7160
Cristina Matsunaga <criismnaga@gmail.com>
2025-05-26 15:26:03 -0300

    Adição de data annotations, Adição de AddJsonOptions no builder do controller

--- Models/Plant.cs
+++ Models/Plant.cs
@@ -1,12 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace IndoorPlantsC_.Models;
 
 public class Plant
 {
+    [Key]
     public int PlantId { get; set; }
+    [StringLength(80)]
     public string CientificName { get; set; }
+    [StringLength(80)]
     public string PopularName { get; set; }
+    [StringLength(80)]
     public string Family { get; set; }
-    public bool Ispoisonous { get; set; }
+    public bool IsPoisonous { get; set; }
     public int WateringFrequency { get; set; }
 
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace IndoorPlantsC_.Models;
 
 public class User
 {
+    [Key]
     public int UserId { get; set; }
+    [StringLength(80)]
     public string Name { get; set; }
+    [StringLength(80)]
     public string LastName { get; set; }
+    [StringLength(80)]
     public DateTime BirthdayDate { get; set; }
+    [StringLength(80)]
     public string ResidenceType { get; set; }
+    [Column(TypeName = "decimal(10,2)")]
     public double AvailableTime { get; set; }
     public int ExperiencELevel { get; set; }
 }
